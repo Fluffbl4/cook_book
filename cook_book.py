ae49d42209eee273c277daf50210b548bd20474c
Sergey Karyukhin <prajs601@yandex.ru>
2024-10-10 13:48:31 +0300

    Added Task 2 in cook_book.py

--- cook_book.py
+++ cook_book.py
@@ -12,6 +12,16 @@ with open('recipes.txt', encoding='utf-8') as file:
         cook_book[recepie_name] = ingredients
 #print(f'cook_book = {cook_book}')
 
-def get_shop_list_by_dishes(dishes, person_count):
+def get_shop_list_by_dishes(dishes: list, person_count: int):
     result = {}
-    
+    for dish in dishes:
+        if dish in cook_book:
+            for consist in cook_book[dish]:
+                if consist['product'] in result:
+                    result[consist['product']]['quantity'] += int(consist['quantity']) * int(person_count)
+                else:
+                    result[consist['product']] = {'measure': consist['measure'],'quantity': int(consist['quantity']) * int(person_count)}
+        else:
+            print('Такого блюда нет в книге')
+    print(result)
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
