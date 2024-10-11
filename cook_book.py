54e4d3053696faa1089ff6afde900d81adfd9129
Sergey Karyukhin <prajs601@yandex.ru>
2024-10-11 11:05:31 +0300

    Added task3 in cook_book.py

--- cook_book.py
+++ cook_book.py
@@ -1,3 +1,6 @@
+import os
+import os.path
+
 with open('recipes.txt', encoding='utf-8') as file:
     cook_book = {}
     for i in file:
@@ -24,4 +27,37 @@ def get_shop_list_by_dishes(dishes: list, person_count: int):
         else:
             print('Такого блюда нет в книге')
     print(result)
-get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+#get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
+def count_lines(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        return sum(1 for line in f if line.strip())
+
+
+def rewrite_files(file_for_writing: str, base_path, location):
+    # Перебираем все файлы в подпапке и считаем количество строк в каждом
+    files = [(count_lines(os.path.join(base_path, location, filename)), os.path.join(base_path, location, filename),
+              filename) \
+             for filename in os.listdir(os.path.join(base_path, location))]
+
+    # Сортируем файлы по количеству строк
+    sorted_files = sorted(files, key=lambda x: x[0])
+
+    # Открываем итоговый файл для записи
+    with open(file_for_writing, 'w', encoding='utf-8') as result_file:
+        for _, path, filename in sorted_files:
+            # Записываем служебную информацию
+            result_file.write(f'{filename}\n{count_lines(path)}\n')
+
+            # Читайте и записывайте содержимое файла построчно
+            with open(path, 'r', encoding='utf-8') as input_file:
+                result_file.write(input_file.read())
+                result_file.write('\n')
+
+# Определяем пути
+file_for_writing = os.path.abspath('\\Users\\prajs\\Desktop\\cook_book\\1.txt')
+base_path = os.getcwd()
+location = os.path.abspath('\\Users\\prajs\\Desktop\\cook_book\\files_to_task_3')
+
+# Вызываем функцию для объединения файлов
+rewrite_files(file_for_writing, base_path, location)
